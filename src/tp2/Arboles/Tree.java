6e079598cad7e9c62a6d0598ced3cf8bc058c700
Pablo Aguirre <pabloaaguirrejob@gmail.com>
2026-04-01 18:10:49 -0300

    cree la carpeta tp2.Arboles con Tree y TreeNode

---
+++ src/tp2/Arboles/Tree.java
@@ -0,0 +1,9 @@
+package tp2.Arboles;
+
+public class Tree {
+    private TreeNode raiz;
+    public Tree(){
+        this.raiz = null;
+    }
+
+}
---
+++ src/tp2/Arboles/TreeNode.java
@@ -0,0 +1,13 @@
+package tp2.Arboles;
+
+public class TreeNode {
+    private int valor;
+    private TreeNode izquierda;
+    private TreeNode derecha;
+
+    public TreeNode(int value){
+        this.valor = value;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+}
