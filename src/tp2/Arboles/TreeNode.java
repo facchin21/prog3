cec7d204c0e8c2a638951ad956f4b51a4925c278
facchin21 <ferminfacchinq@gmail.com>
2026-04-02 16:30:26 -0300

    Refactor Tree and TreeNode classes to implement binary tree structure with add method for inserting values.

--- src/tp2/Arboles/TreeNode.java
+++ src/tp2/Arboles/TreeNode.java
@@ -1,13 +1,37 @@
 package tp2.Arboles;
 
-public class TreeNode {
-    private int valor;
-    private TreeNode izquierda;
-    private TreeNode derecha;
 
-    public TreeNode(int value){
-        this.valor = value;
-        this.izquierda = null;
-        this.derecha = null;
-    }
+public class TreeNode {
+
+	private Integer value;
+	private TreeNode left;
+	private TreeNode right;
+
+	public TreeNode(Integer value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	public TreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+
 }
